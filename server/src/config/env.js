2ec1b8880186d206575dfce65b4192dc57f4f5c5
Aktar9775 <aktar.s12129@gmail.com>
2026-01-17 13:55:43 +0530

    fix cors origin in netlify

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -5,8 +5,7 @@ export const env = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
   mongoUri: process.env.MONGO_URI,
-  clientUrl: (process.env.CLIENT_URL || "").replace(/\/$/, ""),
-
+  clientUrl: process.env.CLIENT_URL ,
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
   accessExpires: process.env.ACCESS_TOKEN_EXPIRES || "15m",
