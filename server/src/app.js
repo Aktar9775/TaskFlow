0dd4820f68529076181c2fde42e6795382871a2c
Aktar9775 <aktar.s12129@gmail.com>
2026-01-17 14:03:15 +0530

    fix cors origin in netlify

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,22 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import routes from "./routes/index.js";
+import { env } from "./config/env.js";
+
+const app = express();
+
+app.use(helmet());
+
+// ✅ FIXED CORS (no mismatch + supports local)
+const allowedOrigins = [
+  env.clientUrl,
+  "http://localhost:5173",
+  "http://localhost:3000",
+].filter(Boolean);
+
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -20,3 +39,16 @@ app.use(
 
 // ✅ Fix preflight
 app.options("/*", cors());
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("TaskFlow Backend is running");
+});
+
+app.use("/api", routes);
+
+export default app;
